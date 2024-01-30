342f1c33fed3e200481281110272f1b28db67abb
Lilia1976 <104726291+Lilia1976@users.noreply.github.com>
2024-01-30 19:59:26 +0200

    DZ7
    DZ-7

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+/*напиши скрипт, який:
+Порахує й виведе в консоль кількість категорій в 
+ul#categories, тобто елементів li.item.
+Для кожного елемента li.item у списку ul#categories 
+знайде й виведе в консоль текст заголовка елемента 
+(тегу <h2>) і кількість елементів у категорії (усіх <li>,
+вкладених у нього).*/
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach(item => {
+    const categoryName = item.querySelector('h2').textContent;
+    const categoryElements = item.querySelectorAll('li').length;
+
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elemens: ${categoryElements}`)
+});
---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+/* Напиши скрипт, який під час набору тексту в інпуті input#name-input 
+(подія input) підставляє його поточне значення в span#name-output як 
+ім’я для привітання. Обов’язково очищай значення в інпуті по краях від 
+пробілів . Якщо інпут порожній або містить лише пробіли, то замість 
+імені у спан має підставлятися рядок "Anonymous"*/
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', handleInput);
+
+function handleInput() {
+  const inputValue = nameInput.value.trim(); // Очищаємо від пробілів по краях
+    if (inputValue === '') {
+    nameOutput.textContent = 'Anonymous';
+    } else {
+    nameOutput.textContent = inputValue;
+    }
+}
